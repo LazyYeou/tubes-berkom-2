ab19aaf5d9cc6e273ff86e58b65c2796d6a7ef50
LazyYeou <lazyyeou@gmail.com>
2025-12-04 22:12:01 +0700

    feat: add new_max function

--- custom_function.py
+++ custom_function.py
@@ -12,4 +12,9 @@ class CustomFunction:
                 min = i
         return min
     
-    
+    def new_max(x):
+        max = x[0]
+        for i in x:
+            if i > max:
+                max = i
+        return max
