293d3e9ffbaa18de5c0f59e1ec428569cb27c379
Matthew R <spazmash1101@gmail.com>
2025-12-06 19:03:25 +0700

    segment tree fix

--- segment_tree.py
+++ segment_tree.py
@@ -3,7 +3,7 @@
 '''
 
 class product_node:
-    	def __init__(self, name, val, category, qr):
+    def __init__(self, name, val, category, qr):
         self.name = name
         self.Price = val
         self.category = category
@@ -56,34 +56,14 @@ class SegmentTree:
         mid = (left + right)//2
         return self.get_total_sum(node * 2, left, mid, l_range, r_range) + self.get_total_sum(node * 2 + 1, mid+1, right, l_range, r_range)
         
-<<<<<<< HEAD
-    def add(self, node, left, right, val):
-        if left == right:
-            if self.tree[node].minVal <= val.minVal:
-                self.tree[2 * node] = self.tree[node]
-                self.tree[2 * node + 1] = val
-            else :
-                self.tree[2 * node] = val
-                self.tree[2 * node + 1] = self.tree[node]
-        else:
-            if self.tree[node].minVal > val.minVal or self.tree[node].maxVal < val.minval:
-                return
-            mid = (left + right)//2
-            self.add(node * 2, left, mid, val)
-            self.add(node * 2 + 1, mid + 1, right, val)
-        self.tree[node].minVal = min(self.tree[2 * node].minVal, self.tree[2 * node + 1].minVal)
-        self.tree[node].maxVal = max(self.tree[2 * node].maxVal, self.tree[2 * node + 1].maxVal)
-        self.tree[node].sumVal = self.tree[2 * node].sumVal + self.tree[2 * node + 1].sumVal
-=======
     def build(self, node, left, right, prod):
-								if(left == right):
-								    self.tree[node] = prod[left]
+        if(left == right):
+            self.tree[node] = prod[left]
         mid = (left + right)//2
-        build(self, 2 * node, left, mid, prod)
-        build(self, 2 * node + 1, mid + 1, right, prod)
+        self.build(self, 2 * node, left, mid, prod)
+        self.build(self, 2 * node + 1, mid + 1, right, prod)
         self.tree[node].Price = self.tree[2 * node].Price + self.tree[2 * node + 1].Price
   
->>>>>>> 67c0ca995efb1b97a9eff67ece40311f9ca49bcf
 
     
     def get_stats(self):
